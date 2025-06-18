da2633ad82c11db4fbfb0a3bc95be9d29092cf10
sumaiyahibrahim <sumaiyahibrahim15@gmail.com>
2025-06-18 15:54:07 +0530

    index.html created

---
+++ script.js
@@ -0,0 +1,25 @@
+function calculateBMI() {
+  const weight = Number(document.getElementById("weight").value);
+  const height = Number(document.getElementById("height").value);
+  const resultDiv = document.getElementById("result");
+
+  if (weight <= 0 || height <= 0 || isNaN(weight) || isNaN(height)) {
+    resultDiv.innerText = "Please enter valid weight and height!";
+    return;
+  }
+
+  const bmi = weight / (height * height);
+  let category = "";
+
+  if (bmi < 18.5) {
+    category = "Underweight";
+  } else if (bmi < 25) {
+    category = "Normal weight";
+  } else if (bmi < 30) {
+    category = "Overweight";
+  } else {
+    category = "Obese";
+  }
+
+  resultDiv.innerText = `Your BMI is ${bmi.toFixed(2)} (${category})`;
+}
